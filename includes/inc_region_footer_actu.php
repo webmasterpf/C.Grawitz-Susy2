065136ec9cbf83a6055df85736b71b31edaca14b
webmasterpf-DG <webmaster@provence-formation.com>
2016-04-21 12:21:14 +0200

    initialisation du depot
    Signed-off-by: webmasterpf-DG <webmaster@provence-formation.com>

---
+++ includes/inc_region_footer_actu.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+ * Permet de créer une region pour la colonne G1 dans les node.tpl
+ * <p>LA REGION EXISTE</p>
+ */
+
+?>
+
+<?php //regions pour inserer un bloc dans la colonne G1
+if ($actuAssociation): ?>
+<div id="region_footer_actu">
+    <?php  print $actuAssociation; ?>
+</div>
+    <?php endif; ?>
