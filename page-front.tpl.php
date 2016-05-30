393fea8c060f3a72fb47b07b594706c7b663a131
webmasterpf-DG <webmaster@provence-formation.com>
2016-05-30 12:17:42 +0200

    HP: correction du code, remis ene place des blocs

--- page-front.tpl.php
+++ page-front.tpl.php
@@ -1,53 +1,41 @@
-<?php
-$theme_path = drupal_get_path('theme', 'cyrano_cg');
-include ($theme_path.'/includes/inc_header.php');
-?>
 <!-- ______________________ LAYOUT HOMEPAGE PANORAMIQUE C.GRAWITZ_______________________ -->
+<?php
+global $theme_path;
+include ($theme_path.'/includes/inc_header.php');
+?>  
  <!-- ______________________ CONTENU _______________________ -->
-<?php //<div id="mainPage"> ?>
-	<div id="contentPage">
-
 
           <?php if ($content_top): ?>
             <div id="content-top">
               <?php print $content_top; ?>
             </div> <!-- /#content-top -->
           <?php endif; ?>
-
-<?php
-$theme_path = drupal_get_path('theme', 'cyrano_cg');
-include ($theme_path.'/includes/inc_drupalMMHT.php');
-?>          
+       
 
    <!-- ______________________ ZONE DIAPO PANORAMIQUE _______________________ -->
 
-              <?php if ($DiapoHP): ?>
-                 <div id="HPDiapo"><?php print $DiapoHP; ?></div>
+              <?php if ($DiapoPanoHP): ?>
+                 <div class="HPDiapo"><?php print $DiapoPanoHP; ?></div>
               <?php endif; ?>
 
 
 
-
-
-	<br clear="all"/>
-
-
 <!-- ______________________ PARTIE BAS GAUCHE _______________________ -->
-                 <?php if ($HPBasGauche): ?>
-                 <div id="BasGaucheHP"><?php print $HPBasGauche; ?></div>
+                 <?php if ($HPBasG): ?>
+                 <div class="HPBasGauche"><?php print $HPBasG; ?></div>
               <?php endif; ?>
 
 <!-- ______________________ PARTIE BAS DROITE _______________________ -->
 
 
 
-        <?php if ($HPBasDroit): ?>
-     <div id="BasDroitHP"><?php print $HPBasDroit; ?></div>
+        <?php if ($HPBasD): ?>
+     <div class="HPBasDroit"><?php print $HPBasD; ?></div>
         <?php endif; ?>
-     <br clear="all"/>
+  
 	<!-- ______________________ CONTENU BAS _______________________ -->
     <?php if ($content_bottom_home): ?>
-    <div id="content-bottom-home">
+    <div class="content-bottom-home">
       <?php print $content_bottom_home; ?>
         <?php //print $feed_icons; ?>
          <?php endif; ?>
@@ -57,9 +45,7 @@ include ($theme_path.'/includes/inc_drupalMMHT.php');
 
 
 
-	 </div> <!-- /mainPage -->
 <?php
-$theme_path = drupal_get_path('theme', 'cyrano_cg');
+global $theme_path;
 include ($theme_path.'/includes/inc_footer.php');
-?>
-      
+?>      
